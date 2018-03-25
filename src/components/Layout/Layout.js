c7a65fde8aafd70f942d240b722cb652dc885285
sopanda <hushchabogdan@gmail.com>
2018-03-25 18:47:46 +0200

    Created ingredients component and added proptypes validation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
+import classes from './Layout.css';
 
 const layout = ( props ) => (
     <Auxiliary>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className= {classes.Content}>
             {props.children}
         </main>
     </Auxiliary>
