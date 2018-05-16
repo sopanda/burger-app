43141fcbcc1633bd8c169a8068a5d474f075209e
sopanda <hushchabogdan@gmail.com>
2018-05-16 11:43:51 +0200

    deleted orderSummury and created CheckoutSummury with new functionality

--- src/components/Order/CheckoutSummury/CheckoutSummury.js
+++ src/components/Order/CheckoutSummury/CheckoutSummury.js
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Burger from '../../Burger/Burger';
 import {Button, Row, Col, Container} from 'reactstrap';
 import classes from './CheckoutSummury.css';
-import OrderSummury from '../../OrderSummury/OrderSummury';
+import PriceSection from '../../PriceSection/PriceSection';
 
-const checkoutSummury = (props) => {
+class CheckoutSummury extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.ingredients !== this.props.drinks || 
+               nextProps.drinks !== this.props.drinks;
+    }
+
+    render() {
+        let totalOrder = [...this.props.ingredients, ...this.props.drinks];
+
+        /* seaching for duplicates and creating object with founded values and their count */
+        const order = totalOrder.reduce(function(prev, cur) {
+            prev[cur] = (prev[cur] || 0) + 1;
+            return prev;
+          }, {});
+    
+        const orderSummury = Object.keys(order).map((key, i) => 
+            <li key={i}>{key} : x{order[key]}</li>
+        );
+
     return (
             <div className={classes.CheckoutSummury}>
                 <Row>
                     <Col  md="6" sm="6">
                         <div className={classes.CheckoutBurger}>
-                            <Burger ingredients={props.ingredients}/>
+                            <Burger ingredients={this.props.ingredients}/>
                         </div>
                     </Col>
                     <Col  md="6" sm="6">
                         <h3>Your final order: </h3>
-                        <OrderSummury orderIngridients={props.ingredients} total={props.priceOrder} orderDrinks={props.drinks}/>
+                        <ul>
+                            {orderSummury}
+                        </ul>
+                     <PriceSection price={this.props.priceOrder}/>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                        <Button color="success" onClick={props.onCheckoutContinued}>Pay</Button>
-                        <Button color="danger" onClick={props.onCheckoutCancelled}>Cancel</Button>
+                        <Button color="success" onClick={this.props.onCheckoutContinued}>Pay</Button>
+                        <Button color="danger" onClick={this.props.onCheckoutCancelled}>Cancel</Button>
                     </Col>
                 </Row>
             </div>
     );
 }
+}
 
-export default checkoutSummury;
+export default CheckoutSummury;
